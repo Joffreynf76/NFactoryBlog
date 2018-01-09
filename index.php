44739e34bfd543902be0751fde01527b4482ed43
Joffreynf76 <32874652+Joffreynf76@users.noreply.github.com>
2018-01-09 13:03:31 +0100

    Ajout function

--- index.php
+++ index.php
@@ -16,25 +16,8 @@
 <body>
     <div id="main">
     <?php
-    include_once("./include/header.php");
-        if (isset($_GET['page']) && $_GET['page'] != "") {
-            $page = $_GET['page'];    
-        }
-        
-        else {
-            $page = "default";
-        }
-        
-        $page = "./include/" . $page . ".inc.php";
-        $incFiles=glob("./include/*.inc.php");
-        if(in_array($page,$incFiles)){
-            include($page);
-        } else {
-            include("./include/default.inc.php");
-        }
-        
-        
-        include_once("./include/footer.php");
+    include_once("./function/callPage.php");
+
         
     ?>
     </div>
