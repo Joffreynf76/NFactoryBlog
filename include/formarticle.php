0f2065debeded042af6f268ba316a2790c8dea45
Joffreynf76 <32874652+Joffreynf76@users.noreply.github.com>
2018-01-12 14:36:07 +0100

    formulaire article

---
+++ include/formarticle.php
@@ -0,0 +1,17 @@
+<form method="post" action="#">
+    <div class="titre">
+        <label for="titre">Titre : </label><input type="text" name="titre">
+    </div>
+    <div class="chapo">
+        <label for="chapo">Sous titre : </label><input type="text" name="chapo">
+    </div>
+    <div class="contenu">
+        <label for="contenu">Contenu article :</label><input type="textarea" name="contenu">
+    </div>
+    <div class="date">
+        <label for="date">Date : </label><input type="text" name="titre">
+    </div>
+    <div class="bouton">
+        <input type="submit" value="Publier" name="formulaire3">
+    </div>
+</form>
