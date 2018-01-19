cf4ac16806bc4a8f267f51167fad0cd3d8d89095
Joffreynf76 <32874652+Joffreynf76@users.noreply.github.com>
2018-01-19 15:53:22 +0100

    modif role

--- include/admin.inc.php
+++ include/admin.inc.php
@@ -5,7 +5,7 @@ if($_SESSION['admin']==1) {
     $result= mysqli_query($connexion,$requete);
     echo"<table>";
     while ($donnees=mysqli_fetch_array($result)){
-        echo("<tr><td>".$donnees['USERNAME']."</td>"."<td>".$donnees['USERFNAME']."</td>"."<td>".$donnees['USERMAIL']
+        echo("<tr><td>".$donnees['ID_USER']."</td>"."<td>".$donnees['USERNAME']."</td>"."<td>".$donnees['USERFNAME']."</td>"."<td>".$donnees['USERMAIL']
             ."</td>"."<td>".$donnees['T_ROLES_ID_ROLE']."</td>"."<td>"."<form method='post' action='#'>"."<select name='select'><option value='1'>1</option>
             <option value='2'>2</option><option value='3'>3</option><option value='4'>4</option><option value='5'>5</option>
             </select> "."</td>"."<td>"."<input type='text' name='id'>"."<input type='submit' value='Mettre a jour' name='formulaire'>"."</form>"."</td>"."</tr>");
