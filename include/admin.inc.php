eb5c2e59fa3e17a8301fbe72d6f80b4db2a211c0
Joffreynf76 <32874652+Joffreynf76@users.noreply.github.com>
2018-01-19 12:19:54 +0100

    affichage membre

--- include/admin.inc.php
+++ include/admin.inc.php
@@ -1,7 +1,13 @@
 <?php
 if($_SESSION['admin']==1) {
-    echo("<table><thead><tr><td>Nom</td><td>Prénom</td><td>Adresse email</td><td>Rôle</td></tr></thead>");
-
+    $connexion=mysqli_connect("localhost","root","","NFactoryBlog");
+    $requete = "SELECT * FROM t_users";
+    $result= mysqli_query($connexion,$requete);
+    echo"<table>";
+    while ($donnees=mysqli_fetch_array($result)){
+        echo("<tr><td>".$donnees['USERNAME']."</td>"."<td>".$donnees['USERFNAME']."</td>"."<td>".$donnees['USERMAIL']."</td>"."</tr>");
+    }
+    echo("</table>");
 } else {
     echo "erreur";
 }
