7de906824a597fe3c0c9679ce98e77a28ed26448
Joffreynf76 <32874652+Joffreynf76@users.noreply.github.com>
2018-01-25 12:06:45 +0100

    pagination

--- include/accueil.inc.php
+++ include/accueil.inc.php
@@ -3,12 +3,29 @@
 
 echo("<h1>Accueil</h1>");
 $db=connectionPDO('localhost' , 'NFactoryBlog' , 'root' , '');
+$articleParPage=3;
+$retourTotal= $db -> query("SELECT COUNT(*) AS total FROM t_articles");
+$donneesTotal = $retourTotal -> fetch(PDO::FETCH_ASSOC);
+$total = $donneesTotal['total'];
+$nombrePage = ceil($total/$articleParPage);
+
+
+if(isset($_GET['debut'])){
+    $pageActuelle = intval($_GET['debut']);
+
+    if($pageActuelle>$nombrePage){
+        $pageActuelle=$nombrePage;
+    }
+}
+else {
+    $pageActuelle=1;
+}
+
+$premiereEntree = ($pageActuelle-1)*$articleParPage;
 $requete = "SELECT * FROM t_articles LEFT JOIN t_categories_has_t_articles
- ON t_articles.ID_ARTICLE=t_categories_has_t_articles.T_ARTICLES_ID_ARTICLE LEFT JOIN t_categories ON t_categories_has_t_articles.T_CATEGORIEs_ID_CATEGORIE=t_categories.ID_CATEGORIE";
+ON t_articles.ID_ARTICLE=t_categories_has_t_articles.T_ARTICLES_ID_ARTICLE LEFT JOIN t_categories ON t_categories_has_t_articles.T_CATEGORIEs_ID_CATEGORIE=t_categories.ID_CATEGORIE LIMIT $premiereEntree,$articleParPage";
 $result = $db->query($requete);
-
-
-
+//$retourArticle = $db ->query("SELECT * FROM t_articles LIMIT 0,5");
 
  while ($donnees= $result -> fetch()){
      echo "<h2>";
@@ -28,6 +45,20 @@ $result = $db->query($requete);
      echo("<hr>");
 
  }
+echo '<p align="center">Page : ';
+for($i=1; $i<=$nombrePage; $i++)
+{
+
+    if($i==$pageActuelle)
+    {
+        echo ' [ '.$i.' ] ';
+    }
+    else
+    {
+        echo ' <a href="index.php?page=accueil&amp;debut='.$i.'">'.$i.'</a> ';
+    }
+}
+echo '</p>';
 unset($db);
 
 ?>
