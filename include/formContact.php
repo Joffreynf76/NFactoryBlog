fb2e3f8a4a2cafaee39b825e93ca01bbe03e0225
Joffreynf76 <32874652+Joffreynf76@users.noreply.github.com>
2018-01-30 17:19:34 +0100

    css contact

--- include/formContact.php
+++ include/formContact.php
@@ -1,6 +1,14 @@
 <form method="post" action="#">
-    <label for="email">Email :</label><input type="email" name="email">
-    <label for="sujet">Sujet :</label><input type="text" name="sujet">
-    <textarea name="message" placeholder="Votre message"></textarea>
-    <input type="submit" value="Envoyer" name="contact">
+    <div class="email2">
+        <label for="email">Email :</label><input type="email" name="email">
+    </div>
+    <div class="sujet">
+        <label for="sujet">Sujet :</label><input type="text" name="sujet">
+    </div>
+    <div class="message2">
+        <textarea name="message" placeholder="Votre message"></textarea>
+    </div>
+    <div class="btncontact">
+        <input type="submit" value="Envoyer" name="contact">
+    </div>
 </form>
